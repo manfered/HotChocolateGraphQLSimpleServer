692d6b8d57e994a925550d98874e9cedbc1b6957
manfered <farzad.seifi@gmail.com>
2022-01-23 01:18:30 -0700

    Default transaction scope handler

--- HotChocolateGraphQLSimpleServer/Program.cs
+++ HotChocolateGraphQLSimpleServer/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add Hot Chocolate GraphQL server and define the root query type
 builder.Services
         .AddGraphQLServer()
+        .AddDefaultTransactionScopeHandler() // create transaction scopes for mutation requests to wrap these in a transaction that we can control
         .AddQueryType<Query>()
         .AddMutationType<Mutation>();
 
